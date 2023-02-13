9260b59812bd72526d47e7f0320272b0aa59e398
Boguslaw Fries <boguslaw.fries@autopartner.com>
2023-02-13 19:12:21 +0100

    include
    include h

--- tar.h
+++ tar.h
@@ -1,25 +1,3 @@
-/* GNU tar Archive Format description.
-
-   Copyright 1988-2019 Free Software Foundation, Inc.
-
-   This file is part of GNU tar.
-
-   GNU tar is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
-
-   GNU tar is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
-
-/* tar Header Block, from POSIX 1003.1-1990.  */
-
-/* POSIX header.  */
 
 struct posix_header
 {				/* byte offset */
