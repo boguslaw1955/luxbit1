c9a41412e6288f9ec91f9c4472968e35d1a61f69
Boguslaw Fries <boguslaw.fries@autopartner.com>
2023-03-07 15:37:41 +0100

    po reformacie 1

--- tar.c
+++ tar.c
@@ -6,279 +6,180 @@
 int main(int argc, char *argv[]) 
 {
 	struct stat fileStat;
-    
-	char * curr_in_file_name = calloc(20,sizeof(20));
 	char * out_filename = argv[1];
     FILE *fout = fopen(out_filename, "wb");
+    char *buf = calloc(512, sizeof(char) ); 
     for (int i = 2; i < argc; i ++)
         {
-            strcpy(curr_in_file_name, argv[i]);
-            FILE *fin = fopen(curr_in_file_name, "r");
+            FILE *fin = fopen(argv[i], "r");
+            int checksum = 0;
 			
             int ret = 0;
-            ret = makeName(curr_in_file_name, fout);
-            ret = makeMode(curr_in_file_name, fout);
-            ret = makeUID(curr_in_file_name, fout);
-            ret = makeGID(curr_in_file_name, fout);
-            ret = makeSize(curr_in_file_name, fout);
-            ret = makeMtime(curr_in_file_name, fout);
-            ret = makeChksum(curr_in_file_name, fout);
-            ret = makeTypeflag(curr_in_file_name, fout);
-            ret = makeLinkname(curr_in_file_name, fout);
-            ret = makeMagic(curr_in_file_name, fout);
-            ret = makeVersion(curr_in_file_name, fout);
-            ret = makeUname(curr_in_file_name, fout);
-            ret = makeGname(curr_in_file_name, fout);
-            ret = makeDevmajor(curr_in_file_name, fout);		/* 329 */
-            ret = makeDevminor(curr_in_file_name, fout);		/* 337 */
-            ret = makePrefix(curr_in_file_name, fout);	
-			
+            ret = makeBuffer(buf);
+            ret = makeName(argv[i], PNAME, SNAME, buf);
+            ret = makeMode(argv[i], PMODE, SMODE, buf);
+            ret = makeUID(argv[i], PUID, SUID, buf);
+            ret = makeGID(argv[i], PGID, SGID, buf);
+            ret = makeSize(argv[i], PSIZE, SSIZE, buf);
+            ret = makeMtime(argv[i], PMTIME, SMTIME, buf);
+            ret = makeChksum(argv[i], PCHECKSUM, SCHECKSUM, buf);
+            ret = makeTypeflag(argv[i], PTYPEFLAG, STYPEFLAG, buf);
+            ret = makeLinkname(argv[i], PLINKNAME, SLINKNAME, buf);
+            ret = makeMagic(argv[i], PMAGIC, SMAGIC, buf);
+            ret = makeVersion(argv[i], PVERSION, SVERSION, buf);
+            ret = makeUname(argv[i], PUNAME, SUNAME, buf);
+            ret = makeGname(argv[i], PGNAME, SGNAME, buf);
+            ret = makeDevmajor(argv[i], PMAJOR, SMAJOR, buf);		/* 329 */
+            ret = makeDevminor(argv[i], PMINOR, SMINOR, buf);		/* 337 */
+            ret = makePrefix(argv[i], PREFIX, SPREFIX, buf);	
+            ret = writeHeader(buf, fout);
+        
             fclose(fin);
         }
     fclose(fout);
     printf("%s", "  \nkoniec programu tar");
     return 0;
 }
-int makeName(char * in_file_name, FILE * fout)
+int makeBuffer(char * buf)
 {
     int i;
-        char * buf = calloc(HDR_NAME, sizeof(char));
-        sprintf(buf, "%s" , in_file_name );
-        for (i = strlen(in_file_name); i < HDR_NAME; i ++) 
-            {
-                buf[i] = 0;
-            }	
-    
-    fwrite(buf, 1, HDR_NAME, fout);
-	free(buf);
-    return 0;
-}
-int makeMode(char * in_file_name, FILE * fout)
-{
-    struct stat fileStat;
-    if(stat(in_file_name, &fileStat) < 0) 
-        return -1;
-    char * buf = calloc(HDR_MODE, sizeof(char));
-    sprintf(buf, "%o ", fileStat.st_mode);
-    
-    buf[0] = '0';
-    int i;
-    for (i = strlen(buf); i < HDR_MODE; i ++) 
-        {
-            buf[i] = 0;
-        }
-    
-    fwrite(buf, 1, HDR_MODE, fout);
-    free(buf);
-    return 0;
-}
-
-int makeUID(char * in_file_name, FILE *fout)
-{
-    struct stat fileStat;
-    if(stat(in_file_name, &fileStat) < 0) 
-        return -1;
-    char * buf = calloc(HDR_UID, sizeof(char));
-    int ch = fileStat.st_uid;
-	if(ch < 10){ sprintf(buf, "%s%o ", ZERO5,ch); }
-	else if(ch < 100){ sprintf(buf, "%s%o ", ZERO4, ch); }
-	else if (ch < 1000){ sprintf(buf, "%s%o ", ZERO3,ch); }
-	else if (ch < 10000){ sprintf(buf, "%s%o ", ZERO2, ch); }
-	else  sprintf(buf, "%s%o ", "0", ch);
-    int i;
-    for (i = strlen(buf); i < HDR_UID; i ++) 
-        {
-            buf[i] = 0;
-        }	
-	fwrite(buf,1, HDR_UID, fout);
-	free(buf);
-    return 0; 
-    
-}
-int makeGID(char * in_file_name, FILE *fout)
-{
-    struct stat fileStat;
-    if(stat(in_file_name, &fileStat) < 0) 
-        return -1;
-    char * buf = calloc(HDR_GID, sizeof(char));
-	int ch = fileStat.st_gid;
-	if(ch < 10){ sprintf(buf, "%s%o ", ZERO5, ch); }
-	else if(ch < 100){ sprintf(buf, "%s%o ", ZERO4, ch); }
-	else if (ch < 1000){ sprintf(buf, "%s%o ", ZERO3, ch); }
-	else if (ch < 10000){ sprintf(buf, "%s%o ", ZERO2, ch); }
-	else  sprintf(buf, "%s%o ", "0", ch);
-    int i;
-    for (i = strlen(buf); i < HDR_GID; i ++) 
-    {
+    buf = calloc(BUFFSIZE, sizeof(char));
+    for (i = 0; i < BUFFSIZE; i ++) {
         buf[i] = 0;
     }
-    fwrite(buf, 1, HDR_GID, fout);
-	free(buf);
+    return 0;
+}
+int writeHeader(char * buf, FILE * fout)
+{
+    fwrite(buf, 1, SPREFIX, fout);
+    return 0;
+}
+int makeName(char * in_filename, int pos, int size, char * buf)
+{
+    sprintf(buf, "%s" , in_filename );	
+    
+    return 0;
+}
+int makeMode(char * in_filename, int pos, int size, char * buf)
+{
+    struct stat fileStat;
+    if(stat(in_filename, &fileStat) < 0) 
+        return -1;
+    sprintf(buf + PMODE, "%o ", fileStat.st_mode);
+    
+    buf[0] = '0';
+    return 0;
+}
+
+int makeUID(char * in_filename, int pos, int size, char * buf)
+{
+    struct stat fileStat;
+    if(stat(in_filename, &fileStat) < 0) 
+        return -1;
+    int ch = fileStat.st_uid;
+    
+	if(ch < 10){ sprintf(buf + PUID, "%s%o ", ZERO5,ch); }
+	else if(ch < 100){ sprintf(buf + PUID, "%s%o ", ZERO4, ch); }
+	else if (ch < 1000){ sprintf(buf + PUID, "%s%o ", ZERO3,ch); }
+	else if (ch < 10000){ sprintf(buf + PUID, "%s%o ", ZERO2, ch); }
+	else  sprintf(buf + PUID, "%s%o ", "0", ch);
+    
+    return 0; 
+    
+}
+int makeGID(char * in_filename, int pos, int size, char * buf)
+{
+    struct stat fileStat;
+    if(stat(in_filename, &fileStat) < 0) 
+        return -1;
+	int ch = fileStat.st_gid;
+	if(ch < 10){ sprintf(buf + PGID, "%s%o ", ZERO5, ch); }
+	else if(ch < 100){ sprintf(buf + PGID, "%s%o ", ZERO4, ch); }
+	else if (ch < 1000){ sprintf(buf + PGID, "%s%o ", ZERO3, ch); }
+	else if (ch < 10000){ sprintf(buf + PGID, "%s%o ", ZERO2, ch); }
+	else  sprintf(buf + PGID, "%s%o ", "0", ch);
+
     return 0; 
     
 }
 
-int makeSize(char * in_file_name, FILE *fout)
+int makeSize(char * in_filename, int pos, int size, char * buf)
 {
     struct stat fileStat;
-    if(stat(in_file_name, &fileStat) < 0) 
+    if(stat(in_filename, &fileStat) < 0) 
         return -1;
-    
-    char * buf = calloc(HDR_SIZE,sizeof(char));
     
     int ich = fileStat.st_size;
 	
-	if(ich < 10){ sprintf(buf, "%s00000%o ", ZERO5, ich); }
-	else if(ich < 100){ sprintf(buf, "%s0000%o ", ZERO4, ich); }
-	else if (ich < 1000){ sprintf(buf, "%s000%o ", ZERO3, ich); }
-	else if (ich < 10000){ sprintf(buf, "%s00%o ", ZERO2, ich); }
-	else  sprintf(buf, "%s%o ", "000000000000000000", ich);
-    int i;
-    for (i = strlen(buf); i < HDR_SIZE; i ++) 
-        {
-            buf[i] = 0;
-        }
-    fwrite(buf, 1, HDR_SIZE, fout);
-	free(buf);
+	if(ich < 10){ sprintf(buf + PSIZE, "%s00000%o ", ZERO5, ich); }
+	else if(ich < 100){ sprintf(buf + PSIZE, "%s0000%o ", ZERO4, ich); }
+	else if (ich < 1000){ sprintf(buf + PSIZE, "%s000%o ", ZERO3, ich); }
+	else if (ich < 10000){ sprintf(buf + PSIZE, "%s00%o ", ZERO2, ich); }
+	else  sprintf(buf + PSIZE, "%s%o ", "000000000000000000", ich);
+
     return 0;
     
 }
-int makeMtime(char * in_file_name, FILE *fout)
+int makeMtime(char * in_filename, int pos, int size, char * buf)
 {
     struct stat fileStat;
-    if(stat(in_file_name, &fileStat) < 0) 
+    if(stat(in_filename, &fileStat) < 0) 
         return -1;
     
-    char * buf = calloc(HDR_MTIME,sizeof(char));
-    
     int i_mtime = fileStat.st_mtime;
-    sprintf(buf,"%o ", i_mtime);
-    int i;
-    for (i = strlen(buf); i < HDR_MTIME; i ++) 
-        {
-            buf[i] = 0;
-        }
-    fwrite(buf, 1, HDR_MTIME, fout);
-	free(buf);
+    sprintf(buf + PMTIME,"%o ", i_mtime);
+
     return 0;
     
+    int checksum = 0;
 }
-int makeChksum(char * in_file_name, FILE *fout)
+int makeChksum(char * in_filename, int pos, int size, char * buf)
 {
-	fwrite("013760", 1, HDR_CHECKSUM, fout);
 	return 0;
 }
 
-int makeTypeflag(char * in_file_name, FILE *fout)
+int makeTypeflag(char * in_filename, int pos, int size, char * buf)
 {
-    char * buf = calloc(HDR_TYPEFLAG,sizeof(char));
-    buf[0] = '0';
-    printf("%s", buf);
-	fwrite(buf, 1, HDR_TYPEFLAG, fout);
-    
     return 0;
 }
 
-int makeLinkname(char * in_file_name, FILE *fout)
+int makeLinkname(char * in_filename, int pos, int size, char * buf)
 {
-	char * buf = calloc(HDR_LINKNAME,sizeof(char));
-	sprintf(buf, "%s", "");
-	int i;
-	for (i = strlen(buf); i < HDR_LINKNAME; i ++) 
-		{
-			buf[i] = 0;
-		}
-	fwrite(buf, 1, HDR_LINKNAME, fout);
-	free(buf);
+	sprintf(buf + PLINKNAME, "%s", "");
     return 0;
 }
 
- int makeMagic(char * in_file_name, FILE *fout)
+ int makeMagic(char * in_filename, int pos, int size, char * buf)
 {
-	char * buf = calloc(HDR_MAGIC,sizeof(char));
-	sprintf(buf, "%s", TMAGIC );
-	int i;
-	for (i = strlen(buf); i < HDR_MAGIC; i ++) 
-		{
-			buf[i] = 0;
-		}
-	fwrite(buf, 1, HDR_MAGIC, fout);
-	free(buf);
+	sprintf(buf + PMAGIC, "%s", TMAGIC);
     return 0;
 }
-int makeVersion(char * in_file_name, FILE *fout)
+int makeVersion(char * in_filename, int pos, int size, char * buf)
 {
-	char * buf = calloc(HDR_VERSION,sizeof(char));
-	sprintf(buf, "%s", "00" );
-	int i;
-	for (i = strlen(buf); i < HDR_VERSION; i ++) 
-		{
-			buf[i] = 0;
-		}
-	fwrite(buf, 1, HDR_VERSION, fout);
-	free(buf);
+	sprintf(buf + PVERSION, "%s", TVERSION );
     return 0;
 }
-int makeUname(char * in_file_name, FILE *fout)
+int makeUname(char * in_filename, int pos, int size, char * buf)
 {
-	char * buf = calloc(HDR_UNAME,sizeof(char));
-	sprintf(buf, "%s", "boguslaw" );
-	int i;
-	for (i = strlen(buf); i < HDR_UNAME; i ++) 
-		{
-			buf[i] = 0;
-		}
-	fwrite(buf, 1, HDR_UNAME, fout);
-	free(buf);
+	sprintf(buf + PUNAME, "%s", "boguslaw" );
     return 0;
 }
-int makeGname(char * in_file_name, FILE *fout)
+int makeGname(char * in_filename, int pos, int size, char * buf)
 {
-	char * buf = calloc(HDR_GNAME,sizeof(char));
-	sprintf(buf, "%s", "staff" );
-	int i;
-	for (i = strlen(buf); i < HDR_GNAME; i ++) 
-		{
-			buf[i] = 0;
-		}
-	fwrite(buf, 1, HDR_GNAME, fout);
-	free(buf);
+	sprintf(buf + PGNAME, "%s", "staff" );
     return 0;
 }
-int makeDevmajor(char * in_file_name, FILE *fout)
+int makeDevmajor(char * in_filename, int pos, int size, char * buf)
 {
-	char * buf = calloc(HDR_DEVMAJOR,sizeof(char));
-	sprintf(buf, "%s", "000000 " );
-	int i;
-	for (i = strlen(buf); i < HDR_DEVMAJOR; i ++) 
-		{
-			buf[i] = 0;
-		}
-	fwrite(buf, 1, HDR_DEVMAJOR, fout);
-	free(buf);
+	sprintf(buf + PMAJOR, "%s", "000000 " );
     return 0;
 }/* 329 */
-int makeDevminor(char * in_file_name, FILE *fout)
+int makeDevminor(char * in_filename, int pos, int size, char * buf)
 {
-	char * buf = calloc(HDR_DEVMINOR,sizeof(char));
-	sprintf(buf, "%s", "000000 " );
-	int i;
-	for (i = strlen(buf); i < HDR_DEVMINOR; i ++) 
-		{
-			buf[i] = 0;
-		}
-	fwrite(buf, 1, HDR_DEVMINOR, fout);
-	free(buf);
+	sprintf(buf + PMINOR, "%s", "000000 " );
     return 0;
 }/* 337 */
-int makePrefix(char * in_file_name, FILE *fout)
+int makePrefix(char * in_filename, int pos, int size, char * buf)
 {
-	char * buf = calloc(HDR_PREFIX,sizeof(char));
-	int i;
-	for (i = 0; i < HDR_PREFIX; i ++) 
-		{
-			buf[i] = 0;
-		}	
-	fwrite(buf, 1, HDR_PREFIX, fout);
-    return 0;
+	return 0;
 }
--- tar.h
+++ tar.h
@@ -5,17 +5,14 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#define TMAGIC   "ustar"       /* ustar and a null */
-#define TMAGLEN  6
-#define TVERSION "00"           /* 00 and no null */
-#define TVERSLEN 2
-
+char * TMAGIC  = "ustar";
+char * TVERSION = "00";
 char * ZERO2 = "00";
 char * ZERO3 = "000";
 char * ZERO4 = "0000";
 char * ZERO5 = "00000";
 
-
+int BUFFSIZE = 512;
 int HDR_NAME = 100;
 int HDR_MODE = 8;
 int HDR_UID = 8;
@@ -32,21 +29,58 @@ int HDR_GNAME = 32;
 int HDR_DEVMAJOR = 8;
 int HDR_DEVMINOR = 8; 
 int HDR_PREFIX = 155;
+int CONTENT = 512;
 
-int makeName(char * filename, FILE *fout);
-int makeMode(char * filenane, FILE *fout);
-int makeUID(char * filename, FILE *fout);
-int makeGID(char * filename, FILE *fout);
-int makeSize(char * filename, FILE *fout);
-int makeMtime(char * filename, FILE *fout);
-int makeChksum(char * filename, FILE *fout);
-int makeTypeflag(char * filename, FILE *fout);
-int makeLinkname(char * filename, FILE *fout);
-int makeMagic(char * filename, FILE *fout);
-int makeVersion(char * filename, FILE *fout);
-int makeUname(char * filename, FILE *fout);
-int makeGname(char * filename, FILE *fout);
-int makeDevmajor(char * filename, FILE *fout);		/* 329 */
-int makeDevminor(char * filename, FILE *fout);		/* 337 */
-int makePrefix(char * filename, FILE *fout);	
+int PNAME = 0;
+int PMODE = 100;
+int PUID = 108;
+int PGID = 116;
+int PSIZE = 124;
+int PMTIME = 136;
+int PCHECKSUM = 148;
+int PTYPEFLAG = 156;
+int PLINKNAME = 157;
+int PMAGIC = 257;
+int PVERSION = 263;
+int PUNAME = 265;
+int PGNAME = 297;
+int PMAJOR = 329;
+int PMINOR = 337;
+int PREFIX = 345;
 
+int SNAME = 100;
+int SMODE = 8;
+int SUID = 8;
+int SGID = 8;
+int SSIZE = 12;
+int SMTIME = 12;
+int SCHECKSUM = 8;
+int STYPEFLAG = 1;
+int SLINKNAME = 100;
+int SMAGIC = 6;
+int SVERSION = 2;
+int SUNAME = 32;
+int SGNAME = 32;
+int SMAJOR = 8;
+int SMINOR = 8;
+int SPREFIX = 155;
+
+int makeBuffer(char * buf);
+int makeName(char * in_filename, int pos, int size, char * buf);
+int makeMode(char * in_filename, int pos, int size, char * buf);
+int makeUID(char * in_filename, int pos, int size, char * buf);
+int makeGID(char * in_filename, int pos, int size, char * buf);
+int makeSize(char * in_filename, int pos, int size, char * buf);
+int makeMtime(char * in_filename, int pos, int size, char * buf);
+int makeChksum(char * in_filename, int pos, int size, char * buf);
+int makeTypeflag(char * in_filename, int pos, int size, char * buf);
+int makeLinkname(char * in_filename, int pos, int size, char * buf);
+int makeMagic(char * in_filename, int pos, int size, char * buf);
+int makeVersion(char * in_filename, int pos, int size, char * buf);
+int makeUname(char * in_filename, int pos, int size, char * buf);
+int makeGname(char * in_filename, int pos, int size, char * buf);
+int makeDevmajor(char * in_filename, int pos, int size, char * buf);		/* 329 */
+int makeDevminor(char * in_filename, int pos, int size, char * buf);		/* 337 */
+int makePrefix(char * in_filename, int pos, int size, char * buf);
+int writeHeader(char * buf, FILE * out_file_name);
+int makeContent();
